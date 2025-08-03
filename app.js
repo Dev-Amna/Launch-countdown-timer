9b3f101d56b1c9f7d826c354e3dc391fda511c14
Dev-Amna <amnaharaf272@gmail.com>
2025-08-03 12:04:28 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// Set the date 
+const countDownDate = new Date();
+countDownDate.setDate(countDownDate.getDate() + 14);
+
+// Update the count
+const countdownFunction = setInterval(function () {
+    // Get today's date and time
+    const now = new Date().getTime();
+
+    // Find the distance between 
+    const distance = countDownDate - now;
+
+    // Time calculations 
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result
+    document.getElementById("days").innerHTML = days.toString().padStart(2, '0');
+    document.getElementById("hours").innerHTML = hours.toString().padStart(2, '0');
+    document.getElementById("minutes").innerHTML = minutes.toString().padStart(2, '0');
+    document.getElementById("seconds").innerHTML = seconds.toString().padStart(2, '0');
+
+    //  clear interval
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+    }
+}, 1000);
